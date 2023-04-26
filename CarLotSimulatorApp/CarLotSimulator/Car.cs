08ac2bef1d5db8d065e0eb1e831be39e5cc7a378
Zachari Price <ZachariPrice3@gmail.com>
2023-04-25 23:25:44 -0400

    CarLotSim Exercise

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLotSimulator
+{
+    public class Car
+    {
+
+        public Car()
+        {
+        }
+
+        public Car(int year, 
+                   string make, 
+                   string model, 
+                   string engineNoise, 
+                   string honkNoise, 
+                   bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            EngineNoise = engineNoise;
+            HonkNoise = honkNoise;
+            IsDriveable = isDrivable;
+        }
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+        public void MakeEngineNOise(string engineNoise)
+        {
+            Console.WriteLine(engineNoise);
+        }
+
+        public void MakeHonkNoise(string honkNoise)
+        {
+            Console.WriteLine(honkNoise);
+        }
+
+        public void ModelOfVehicle(string model)
+        { 
+            Console.WriteLine(model);
+
+        }
+
+        public void YearOfProduction(int Year)
+        {
+            Console.WriteLine(Year);
+        }
+
+        public void CanItStart(bool IsDriveable)
+        {
+            Console.WriteLine(IsDriveable);
+
+        }
+
+        public void MakeOfVehicle(string Make)
+        {
+            Console.WriteLine(Make);
+        }
+
+        // Other methods
+
+        // public string MakeofVehicle()
+        // {
+        //    return EngineNoise;
+        // }
+
+
+
+    }
+}
--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -5,7 +5,9 @@ namespace CarLotSimulator
     class Program
     {
         static void Main(string[] args)
+ 
         {
+            var lot = new CarLot();
             //TODO
 
             //Create a seperate class file called Car
@@ -15,6 +17,73 @@ namespace CarLotSimulator
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+            var ZachsCar = new Car();
+
+            ZachsCar.Make = "Hyundai";
+            ZachsCar.Year = 2024;
+            ZachsCar.IsDriveable = true;
+            ZachsCar.HonkNoise = "Balooga";
+            ZachsCar.EngineNoise = "Vrooom";
+            ZachsCar.Model = "Sante Fe";
+
+            lot.Cars.Add(ZachsCar);
+
+            //Object Initializer Syntax
+            var ZacharisCar = new Car()
+            {
+                Year = 2023,
+                Make = "Tesla",
+                Model = "Cyber Truck",
+                HonkNoise = "Tetete",
+                EngineNoise = "Swoosh",
+                IsDriveable = true,
+
+
+            };
+
+            lot.Cars.Add(ZacharisCar);
+
+            // Using the constructor to allow parameter values to be placed inside properties
+            var nemisCar = new Car(2038, "BMW", "350", "HonkHonk", "Yeeeeurm", true);
+
+            lot.Cars.Add(nemisCar);
+
+            //Call methods
+            nemisCar.ModelOfVehicle(nemisCar.Model);
+            nemisCar.MakeEngineNOise(nemisCar.EngineNoise);
+            nemisCar.CanItStart(nemisCar.IsDriveable);
+            nemisCar.MakeHonkNoise(nemisCar.HonkNoise);
+            nemisCar.YearOfProduction(nemisCar.Year);
+            nemisCar.MakeOfVehicle(nemisCar.Model);
+
+            Console.WriteLine();
+
+          
+            ZacharisCar.ModelOfVehicle(ZacharisCar.Model);
+            ZacharisCar.MakeEngineNOise(ZacharisCar.EngineNoise);
+            ZacharisCar.CanItStart(ZacharisCar.IsDriveable);
+            ZacharisCar.MakeHonkNoise(ZacharisCar.HonkNoise);
+            ZacharisCar.YearOfProduction(ZacharisCar.Year);
+            ZacharisCar.MakeOfVehicle(ZacharisCar.Model);
+
+            Console.WriteLine();
+
+            ZachsCar.ModelOfVehicle(ZachsCar.Model);
+            ZachsCar.MakeEngineNOise(ZacharisCar.EngineNoise);
+            ZachsCar.CanItStart(ZachsCar.IsDriveable);
+            ZachsCar.MakeHonkNoise(ZachsCar.HonkNoise);
+            ZachsCar.YearOfProduction(ZachsCar.Year);
+            ZachsCar.MakeOfVehicle(ZachsCar.Model);
+
+
+
+            Console.WriteLine();
+
+
+            
+
+
+
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
@@ -28,6 +97,11 @@ namespace CarLotSimulator
             //It should have at least one property: a List of cars
             //Instanciate the a Carlot at the beginning of the program and as you create a car add the car to the list.
             //At the end iterate through the list printing each of car's Year, Make, and Model to the console
+            foreach(var car in lot.Cars)
+            {
+                Console.WriteLine($"Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
         }
+
     }
 }
